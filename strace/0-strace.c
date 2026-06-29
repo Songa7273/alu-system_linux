68149c20aec19ce70f718d99e551c99aa4e668d2
Songa7273 <songa7273@example.com>
2026-06-29 08:59:06 -0700

    initial commit

--- strace/0-strace.c
+++ strace/0-strace.c
@@ -1,77 +1,51 @@
+#include <stdio.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
-#include <sys/user.h>
 #include <sys/wait.h>
+#include <sys/user.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-extern char **environ;
-
 /**
- * trace_syscalls - trace and print syscall numbers
- * @child: pid of child process
- *
- * Return: 0 on success
- */
-int trace_syscalls(pid_t child)
-{
-	int status;
-	struct user_regs_struct regs;
-
-	ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
-
-	while (1)
-	{
-		ptrace(PTRACE_SYSCALL, child, 0, 0);
-		waitpid(child, &status, 0);
-
-		if (WIFEXITED(status))
-			break;
-
-		if (!(WSTOPSIG(status) & 0x80))
-			continue;
-
-		if (ptrace(PTRACE_GETREGS, child, 0, &regs) == -1)
-			exit(1);
-
-		printf("%lld\n", (long long)regs.orig_rax);
-	}
-
-	return (0);
-}
-
-/**
- * main - executes and traces a command
+ * main - traces a process and prints system call numbers as they're called
  * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, 1 on failure
- */
-int main(int argc, char *argv[])
+ * @argv: argument array
+ * @envp: environment parameters
+ * Return: 0 on success | 1 on failure (not enough arguments)
+ * author: Frank Onyema Orji
+ **/
+int main(int argc, char *argv[], char *envp[])
 {
-	pid_t child;
+	int print, status;
+	struct user_regs_struct regs;
+	pid_t pid;
 
 	if (argc < 2)
 	{
-		fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
+		fprintf(stderr, "Usage: %s <full_path> [path_args]\n", argv[0]);
 		return (1);
 	}
 
-	child = fork();
-	if (child == -1)
+	setbuf(stdout, NULL);
+	pid = fork();
+
+	if (pid == 0)
 	{
-		perror("fork");
-		return (1);
+		printf("59\n"); /* execve syscall number */
+		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+		execve(argv[1], argv + 1, envp);
+	}
+	else
+	{
+		for (status = 1, print = 0; !WIFEXITED(status); print ^= 1)
+		{
+			ptrace(PT_SYSCALL, pid, NULL, NULL);
+			wait(&status);
+			ptrace(PT_GETREGS, pid, NULL, &regs);
+			if (print)
+				printf("%lu\n", (size_t)regs.orig_rax);
+		}
 	}
 
-	if (child == 0)
-	{
-		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-		execve(argv[1], &argv[1], environ);
-		perror("execve");
-		exit(1);
-	}
-
-	return (trace_syscalls(child));
+	return (0);
 }
