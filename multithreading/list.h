dc9168fb36f579de246831bd150e13f13b1be44a
Songa7273 <k.gaju@alustudent.com>
2026-07-09 14:36:33 +0200

    initial commit

--- multithreading/list.h
+++ multithreading/list.h
@@ -10,19 +10,19 @@ typedef struct node_s node_t;
 typedef struct list_s list_t;
 typedef void (*node_func_t)(void *);
 
-typedef struct node_s
+struct node_s
 {
 	void *content;
 	node_t *prev;
 	node_t *next;
-} node_t;
+};
 
-typedef struct list_s
+struct list_s
 {
 	size_t size;
 	node_t *head;
 	node_t *tail;
-} list_t;
+};
 
 list_t *list_init(list_t *list);
 node_t *list_add(list_t *list, void *content);
