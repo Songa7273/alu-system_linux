fec9a97e2a429660773d74156377d8822b54fee1
Songa7273 <k.gaju@alustudent.com>
2026-07-09 15:02:11 +0200

    initial commit

--- multithreading/10-blur_portion.c
+++ multithreading/10-blur_portion.c
@@ -2,47 +2,67 @@
 #include <stdlib.h>
 #include "multithreading.h"
 
-static pixel_t blur_pixel(img_t const *img, kernel_t const *kernel, size_t x,
-	size_t y)
+static float kernel_sum(kernel_t const *kernel)
+{
+	size_t i;
+	size_t j;
+	float sum;
+
+	sum = 0.0f;
+	for (i = 0; i < kernel->size; i++)
+	{
+		for (j = 0; j < kernel->size; j++)
+			sum += kernel->matrix[i][j];
+	}
+	return (sum);
+}
+
+static void accumulate_channel(img_t const *img, kernel_t const *kernel,
+	size_t x, size_t y, float *red, float *green, float *blue)
 {
 	size_t half;
 	size_t i;
 	size_t j;
-	float kernel_sum;
-	float red;
-	float green;
-	float blue;
 	long px;
 	long py;
 	float weight;
-	pixel_t pixel;
 
 	half = kernel->size / 2;
-	kernel_sum = 0.0f;
-	red = 0.0f;
-	green = 0.0f;
-	blue = 0.0f;
 	for (i = 0; i < kernel->size; i++)
 	{
 		for (j = 0; j < kernel->size; j++)
 		{
-			kernel_sum += kernel->matrix[i][j];
 			px = (long)x + (long)j - (long)half;
 			py = (long)y + (long)i - (long)half;
 			if (px < 0 || py < 0 || px >= (long)img->w || py >= (long)img->h)
 				continue;
 			weight = kernel->matrix[i][j];
-			kernel_sum += weight;
-			red += img->pixels[py * (long)img->w + px].r * weight;
-			green += img->pixels[py * (long)img->w + px].g * weight;
-			blue += img->pixels[py * (long)img->w + px].b * weight;
+			*red += img->pixels[py * (long)img->w + px].r * weight;
+			*green += img->pixels[py * (long)img->w + px].g * weight;
+			*blue += img->pixels[py * (long)img->w + px].b * weight;
 		}
 	}
-	if (kernel_sum != 0.0f)
+}
+
+static pixel_t blur_pixel(img_t const *img, kernel_t const *kernel, size_t x,
+	size_t y)
+{
+	float sum;
+	float red;
+	float green;
+	float blue;
+	pixel_t pixel;
+
+	sum = kernel_sum(kernel);
+	red = 0.0f;
+	green = 0.0f;
+	blue = 0.0f;
+	accumulate_channel(img, kernel, x, y, &red, &green, &blue);
+	if (sum != 0.0f)
 	{
-		red /= kernel_sum;
-		green /= kernel_sum;
-		blue /= kernel_sum;
+		red /= sum;
+		green /= sum;
+		blue /= sum;
 	}
 	if (red < 0.0f)
 		red = 0.0f;
